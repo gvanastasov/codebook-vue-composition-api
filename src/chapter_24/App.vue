245de23040d083573569b893ebb64bc825e8e8e1
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2024-06-14 11:20:40 +0200

    add transition group example

--- src/chapter_24/App.vue
+++ src/chapter_24/App.vue
@@ -5,11 +5,29 @@
       The AnimateFade component is a wrapper for the content that we want to animate.
       The content will fade in and out when the button is clicked. See the component 
       for more details.
+
+      The content inside Transition component must always be either:
+        - conditionally rendered using v-if or v-show
+        - keyed using the key attribute
     -->
     <div :style="{ position: 'relative' }">
       <AnimateFade :style="{ position: 'absolute' }">
         <span :key="count">{{ count }}</span>
       </AnimateFade>
+    </div>
+
+    <br />
+
+    <!-- 
+      When dealing with more than one element, we can use the TransitionGroup component.
+      The main difference here being that the TransitionGroup component wraps a list of
+      elements that are being added or removed from the DOM, in esssense animting a group
+      of elements.
+    -->
+    <div>
+      <AnimateFadeCollection :items="items" />
+      <button @click="addRandomItem">Add</button>
+      <button @click="removeRandomItem">Remove</button>
     </div>
   </div>
 </template>
@@ -17,9 +35,22 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import AnimateFade from './components/a-fade.vue'
+import AnimateFadeCollection from './components/a-fade-collection.vue'
 
 const count = ref(0)
 setInterval(() => count.value++, 1000)
+
+const items = ref([1, 2, 3, 4, 5])
+let lastIndex = items.value.length
+const addRandomItem = () => {
+  lastIndex++
+  const randomIndex = Math.floor(Math.random() * (items.value.length + 1))
+  items.value.splice(randomIndex, 0, lastIndex)
+}
+const removeRandomItem = () => {
+  const randomIndex = Math.floor(Math.random() * items.value.length)
+  items.value.splice(randomIndex, 1)
+}
 </script>
 
 <style scoped>
---
+++ src/chapter_24/components/a-fade-collection.vue
@@ -0,0 +1,29 @@
+<template>
+  <TransitionGroup name="fade-collection" tag="ul">
+    <li v-for="item in items" :key="item">
+      <slot :data="item">{{ item }}</slot>
+    </li>
+  </TransitionGroup>
+</template>
+
+<script setup lang="ts">
+import type { PropType } from 'vue'
+defineProps({
+  items: {
+    type: Array as PropType<any[]>,
+    required: true
+  }
+})
+</script>
+
+<style>
+.fade-collection-enter-active,
+.fade-collection-leave-active {
+  transition: all 0.5s ease;
+}
+.fade-collection-enter-from,
+.fade-collection-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+</style>
